fe28965255c4a3f33d211a9f3b1256e8e5ef2a56
Rafael Guedes <r.guedes@metocean.co.nz>
2017-06-21 12:38:54 +1200

    Adding src to ftn_msl - no modification yet apart from appliying patch A and replacing macros.inc

--- swan_src/ftn_msl/switch.pl
+++ swan_src/ftn_msl/switch.pl
@@ -1,6 +1,7 @@
 # --- parsing arguments
 $esmf = "FALSE";
 $tim = "FALSE";
+$jac = "FALSE";
 $mpi = "FALSE";
 $pun = "FALSE";
 $f95 = "FALSE";
@@ -11,12 +12,14 @@ $sgi = "FALSE";
 $imp = "FALSE";
 $cvi = "FALSE";
 $adc = "FALSE";
+$coh = "FALSE";
+$ncf = "FALSE";
 $mv4 = "FALSE";
-$ncom = "FALSE";
 while ( $ARGV[0]=~/-.*/ )
    {
    if ($ARGV[0]=~/-esmf/) {$esmf="TRUE";shift;}
    if ($ARGV[0]=~/-timg/) {$tim="TRUE";shift;}
+   if ($ARGV[0]=~/-jac/) {$jac="TRUE";shift;}
    if ($ARGV[0]=~/-mpi/) {$mpi="TRUE";shift;}
    if ($ARGV[0]=~/-pun/) {$pun="TRUE";shift;}
    if ($ARGV[0]=~/-f95/) {$f95="TRUE";shift;}
@@ -27,8 +30,9 @@ while ( $ARGV[0]=~/-.*/ )
    if ($ARGV[0]=~/-impi/) {$imp="TRUE";shift;}
    if ($ARGV[0]=~/-cvis/) {$cvi="TRUE";shift;}
    if ($ARGV[0]=~/-adcirc/) {$adc="TRUE";shift;}
+   if ($ARGV[0]=~/-coh/) {$coh="TRUE";shift;}
+   if ($ARGV[0]=~/-netcdf/) {$ncf="TRUE";shift;}
    if ($ARGV[0]=~/-matl4/) {$mv4="TRUE";shift;}
-   if ($ARGV[0]=~/-ncom/) {$ncom="TRUE";shift;}
    }
 
 # --- make a list of all files
@@ -66,6 +70,8 @@ foreach $file (@files)
       if ($esmf=~/TRUE/) {$newline=~s/^!ESMF//;}
       else               {$newline=~s/^!!ESMF//;} #second "!" is negation
       if ($tim=~/TRUE/) {$newline=~s/^!TIMG//;}
+      if ($jac=~/TRUE/) {$newline=~s/^!JAC//;}
+      else              {$newline=~s/^!WFR//;}
       if ($mpi=~/TRUE/) {$newline=~s/^!MPI//;}
       if ($pun=~/TRUE/) {$newline=~s/^!PUN//;}
       if ($pun=~/FALSE/) {$newline=~s/^!NPUN//;}
@@ -78,10 +84,12 @@ foreach $file (@files)
       if ($cvi=~/TRUE/) {$newline=~s/^!CVIS//;}
       if ($adc=~/TRUE/) {$newline=~s/^!ADC//;}
       if ($adc=~/FALSE/) {$newline=~s/^!NADC//;}
+      if ($coh=~/TRUE/) {$newline=~s/^!COH//;}
+      if ($coh=~/FALSE/){$newline=~s/^!NCOH//;}
+      if ($ncf=~/TRUE/) {$newline=~s/^!NCF//;}
+      if ($ncf=~/FALSE/){$newline=~s/^!NNCF//;}
       if ($mv4=~/TRUE/) {$newline=~s/^!MatL4//;}
       if ($mv4=~/FALSE/) {$newline=~s/^!MatL5//;}
-      if ($ncom=~/TRUE/) {$newline=~s/^!NCOM//;}
-      if ($ncom=~/FALSE/) {$newline=~s/^!SWAN//;}
       print OUTFILE $newline;
     }
     close file;
