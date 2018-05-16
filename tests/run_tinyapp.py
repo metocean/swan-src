b4f114273b2d2910292d354beaafdc9d507dac22
Henrique Rapizo <h.rapizo@meteo.co.nz>
2018-05-16 15:41:16 +1200

    finish script to compare results of docker test

--- tests/run_tinyapp.py
+++ tests/run_tinyapp.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
+import logging
 from datetime import datetime
+from nco import Nco
 
 MODELDIR = '/source/swan-src/tests/tinyapp'
 os.chdir(MODELDIR)
@@ -14,6 +16,21 @@ os.chdir(TESTDIR)
 os.system('rm -rf out/*') # copy test files to new folder
 os.system('mpiexec -n 2 swan.exe par.20180513_00z.swn')
 
-# TODO: compare files with old/original ones using nco/ncdiff for: 
-# hmax, hmin, tmax, tmin, hmean, tmean. Create flag if differences are above certain threshold
-# ...
+# compare results from newly build model with old one
+oldfile  = '../out/par.20180513_00z.nc'
+newfile  = './out/par.20180513_00z.nc'
+diffile  = './dif.nc'
+rdiffile = './reldif.nc'
+maxfile  = './maxdif.nc'
+command  = ('ncdiff', oldfile, newfile, diffile) 
+subprocess.call(command)
+command  = ('ncbo','--op_typ=/', diffile, oldfile, rdiffile)
+subprocess.call(command)
+
+# define min/max/avg values of the difference
+nco = Nco()
+hsmax = nco.ncwa(input=rdiffile, op_typ='max', variable=['hs','tp'], output=maxfile, returnArray='hs').data
+tpmax = nco.ncwa(input=rdiffile, op_typ='max', variable=['hs','tp'], output=maxfile, returnArray='tp').data
+
+if hsmax>0.05: logging.warning(' Maximum Relative Hs difference = %g %%' %(hsmax*100))
+if tpmax>0.05: logging.warning(' Maximum Relative Tp difference = %g %%' %(tpmax*100))
