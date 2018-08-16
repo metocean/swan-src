befad48452e019424f5c9ff6b373bdb34b121c68
rafa-guedes <r.guedes@metocean.co.nz>
2018-08-16 20:46:34 -0300

    Fixed src testing

--- tests/run_tinyapp.py
+++ tests/run_tinyapp.py
@@ -2,39 +2,53 @@ import os
 import subprocess
 import logging
 import shutil
+from collections import OrderedDict
 from datetime import datetime
-from nco import Nco
+from nco import Nco, custom
+
+nco = Nco()
 
 BASEDIR = '/home/metocean'
-TESTDIR = os.path.join(BASEDIR, 'newrun')
-CTRLDIR = os.path.join(BASEDIR, 'tinyapp')
+TESTDIR = os.path.join(BASEDIR, 'tinyapp')
 TARBALL = os.path.join(BASEDIR, 'tinyapp.tar.gz')
 
 os.system('tar -xzvf {} -C {}'.format(TARBALL, BASEDIR))
-shutil.copytree(CTRLDIR, TESTDIR)
 os.chdir(TESTDIR)
 
-os.system('rm -rf out/*') # copy test files to new folder
-jobstr = 'mpiexec -n 2 swan.exe par.20180513_00z.swn'
+jobstr = 'mpiexec -n 2 swan.exe par.20170101_00z_msl.swn'
 os.system(jobstr)
 
+oldfile  = os.path.join(TESTDIR, 'out/grid_out_native.nc')
+newfile  = os.path.join(TESTDIR, 'out/grid_out_msl.nc')
+diffile  = os.path.join(TESTDIR, 'dif.nc')
+rdiffile = os.path.join(TESTDIR, 'reldif.nc')
+
+# Rename variables in old file
+var_mapping = OrderedDict((
+    ('theta0', 'thetam'),
+))
+dim_mapping = OrderedDict((
+    ('lon', 'longitude'),
+    ('lat', 'latitude'),
+))
+nco.ncrename(input=oldfile, options=[
+    custom.Rename("variable", var_mapping),
+    # custom.Rename("dimension", dim_mapping)
+])
+
 # compare results from newly build model with old one
-oldfile  = os.path.join(CTRLDIR, 'out/par.20180513_00z.nc')
-newfile  = os.path.join(TESTDIR, './out/par.20180513_00z.nc')
-diffile  = os.path.join(TESTDIR, './dif.nc')
-rdiffile = os.path.join(TESTDIR, './reldif.nc')
 command  = ('ncdiff', oldfile, newfile, diffile) 
 subprocess.call(command)
 command  = ('ncbo','--op_typ=/', diffile, oldfile, rdiffile)
 subprocess.call(command)
 
 # define min/max/avg values of the difference
-nco = Nco()
-varnames = ['hs','tp','dpm','ugrd10m','vgrd10m','hs_sw','tp_sw','dpm_sw']
+varnames = ['xcur','ycur','xwnd','ywnd','depth','hs','tps','thetam','tm01','tm02']
+
 for varname in varnames:
     print('...checking: {}'.format(varname))
     maxfile  = './maxdif-{}.nc'.format(varname)
     varmax = nco.ncwa(input=rdiffile, op_typ='max', variable=[varname], output=maxfile, returnArray=varname).data
 
-    if varmax > 0.05:
+    if varmax > 0.01:
         raise Exception('Maximum Relative {} difference = {:g} %'.format(varname.title(), varmax*100))
