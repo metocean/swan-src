6e85e64309cc85a3173de793bfdd3efa3bd60287
Henrique Rapizo <h.rapizo@meteo.co.nz>
2018-08-21 14:35:47 +1200

    remove old file to run test

--- tests/runmodel.sh
+++
@@ -1,6 +0,0 @@
-cd /home/metocean/
-tar -xzvf /home/metocean/tinyapp.tar.gz  -C /home/metocean
-cp -a /home/metocean/tinyapp /home/metocean/swan-ori
-cd /home/metocean/swan-ori
-rm -rf out/*
-mpiexec -n 2 swan.exe par.20180513_00z.swn
