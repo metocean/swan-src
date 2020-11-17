a65a9e8533a5e2601b74f929eee7dcb21f5ec933
hrapizo <h.rapizo@metocean.co.nz>
2020-11-18 00:48:16 +1300

    make some changes: comp is working but looks like netcdf-fortran is not good and breaking swan compilation

--- install_scripts/install_swan.sh
+++ install_scripts/install_swan.sh
@@ -3,6 +3,11 @@
 echo -e
 
 echo "----------------- Building SWAN -----------------" 
+# # below if compiling with ifort
+# source /opt/intel/bin/iccvars.sh intel64
+# source /opt/intel/bin/ifortvars.sh intel64
+# source /opt/intel/bin/compilervars.sh intel64
+
 INSTALL_DIR=/usr/local/bin/swan
 mkdir $INSTALL_DIR
 echo "SWAN install dir: $INSTALL_DIR"
@@ -10,13 +15,13 @@ rm $SWAN_SRC/ftn_$FTN/macros.inc
 ln -s $SWAN_SRC/ftn_$FTN/macros/gfortran_static_macros.inc $SWAN_SRC/ftn_$FTN/macros.inc
 
 # Building MPI and Serial versions (OMP not working for some reason)
-for mode in omp ser; do
+for mode in ser mpi omp; do
     echo "Building $FTN version of SWAN in $mode mode"
     cd $SWAN_SRC/ftn_$FTN
     make clobber
     (make $mode 2>&1) | tee build_$mode.log
     mv swan.exe $INSTALL_DIR/swan_$mode.exe
-    if [ $mode == 'omp' ]; then
+    if [ $mode == 'mpi' ]; then
         mv hcat.exe $INSTALL_DIR/
         chmod 777 swanrun && mv swanrun $INSTALL_DIR/
     fi
