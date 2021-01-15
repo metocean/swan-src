fb020b6a7b84e4ba75ce0454a41a86c1dbc9b3dd
Alex Port <a.port@metocean.co.nz>
2021-01-15 15:31:22 +1300

    Use ifort .sh and macros in install_scripts

--- install_scripts/install_swan-ref.sh
+++ install_scripts/install_swan-ref.sh
@@ -5,10 +5,12 @@ echo -e
 echo "----------------- Building SWAN REFERENCE SRC -----------------" 
 INSTALL_DIR=/usr/local/bin/swan
 echo "SWAN install dir: $INSTALL_DIR"
-rm $SWAN_SRC/ftn_msl/macros.inc
-ln -s $SWAN_SRC/ftn_msl/macros/gfortran_static_macros.inc $SWAN_SRC/ftn_msl/macros.inc
-rm $SWAN_SRC/ftn_stock/macros.inc
-ln -s $SWAN_SRC/ftn_msl/macros/gfortran_static_macros.inc $SWAN_SRC/ftn_stock/macros.inc
+#rm $SWAN_SRC/ftn_msl/macros.inc
+#ln -s $SWAN_SRC/ftn_msl/macros/gfortran_static_macros.inc $SWAN_SRC/ftn_msl/macros.inc
+#rm $SWAN_SRC/ftn_stock/macros.inc
+#ln -s $SWAN_SRC/ftn_msl/macros/gfortran_static_macros.inc $SWAN_SRC/ftn_stock/macros.inc
+
+source /opt/intel/bin/compilervars.sh intel64
 
 # Building MPI and Serial versions (OMP not working for some reason)
 for mode in mpi ser; do
