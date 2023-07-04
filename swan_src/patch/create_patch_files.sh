18dfac26572879a5eac69407268e9aaea70d13b3
hrapizo <h.rapizo@metocean.co.nz>
2023-07-04 16:26:52 +1200

    scripts to update src code

---
+++ swan_src/patch/create_patch_files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+BASEDIR=/source/swan-src/swan_src
+stock_dir=$BASEDIR/ftn_stock
+msl_dir=$BASEDIR/ftn_msl
+diff_dir=$BASEDIR/patch
+
+## -------- Generate patch files for all modified files:
+files=("agioncmd.ftn90" "nctablemd.ftn90" "ocpids.ftn" "swanmain.ftn" "swanout1.ftn" \
+       "SwanSpectPart.ftn" "swmod1.ftn" "swn_outnc.ftn90")
+for f in ${files[@]}; do
+  echo "Creating patch for file $f"
+  diff -u -Z -B -p15 $stock_dir/$f $msl_dir/$f > ${diff_dir}/${f}.patch.2
+done
---
+++ update_src.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e
+
+# create new branch for src update
+git checkout -b "version_update"
+
+BASEDIR=/source/swan-src/swan_src
+
+SWAN_VERSION=4145
+mkdir $BASEDIR/ftn_new
+cd $BASEDIR/ftn_new
+echo "Downloading swan version $SWAN_VERSION"
+wget https://swanmodel.sourceforge.io/download/zip/swan$SWAN_VERSION.tar.gz
+tar xpfz swan$SWAN_VERSION.tar.gz
+mv swan$SWAN_VERSION/* .
+rm -rf swan$SWAN_VERSION*
+
+echo
+echo "Getting individual patch of the following modified MSL files:"
+bash $BASEDIR/patch/create_patch_files.sh
+
+echo
+echo "Replacing ftn_msl with new files from ftn_new"
+cp -a $BASEDIR/ftn_msl $BASEDIR/ftn_msl_old # save old msl for tmp backup
+rm -rf $BASEDIR/ftn_msl/*
+cp -rf $BASEDIR/ftn_msl_old/macros/ $BASEDIR/ftn_msl/ # readd macros options
+ln -s $BASEDIR/ftn_msl/macros/nvidia_static_macros.inc $BASEDIR/ftn_msl/macros.inc
+for f in $(ls $BASEDIR/ftn_new/*); do
+    fname=$(basename $f)
+    cp $BASEDIR/ftn_new/$fname $BASEDIR/ftn_msl/$fname
+done
+
+echo
+echo "Applying patch of MSL vs previous stock to new files"
+# WARNING: this will usually fail for swanmain.ftn if new vars are added in the official release
+for f in $(ls $BASEDIR/patch/*ftn*.patch); do
+    fname=$(basename $f)
+    patch -p0 -u $BASEDIR/ftn_msl/${fname%.*} $BASEDIR/patch/$fname
+done
+
+# NOTE: after the above step it might be necessary to fix patch conflixts
+# that's why we do not remove/cleanup any directories. Otherwise:
+# rm -rf $BASEDIR/ftn_stock
+# mv $BASEDIR/ftn_new $BASEDIR/ftn_stock
+# rm -rf $BASEDIR/ftn_msl_old
+
+echo
+git status
+echo "Finished."
