94499a08282ae4da18f4c84077d4ef09658d3599
hrapizo <h.rapizo@metocean.co.nz>
2023-07-04 16:33:45 +1200

    change patch files extension (typo)

--- swan_src/patch/create_patch_files.sh
+++ swan_src/patch/create_patch_files.sh
@@ -9,5 +9,5 @@ files=("agioncmd.ftn90" "nctablemd.ftn90" "ocpids.ftn" "swanmain.ftn" "swanout1.
        "SwanSpectPart.ftn" "swmod1.ftn" "swn_outnc.ftn90")
 for f in ${files[@]}; do
   echo "Creating patch for file $f"
-  diff -u -Z -B -p15 $stock_dir/$f $msl_dir/$f > ${diff_dir}/${f}.patch.2
+  diff -u -Z -B -p15 $stock_dir/$f $msl_dir/$f > ${diff_dir}/${f}.patch
 done
