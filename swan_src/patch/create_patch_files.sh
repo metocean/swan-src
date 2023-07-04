8d749a3e48da4a4104ea120384af9c7a5ce97411
hrapizo <h.rapizo@metocean.co.nz>
2023-07-04 15:51:23 +1200

    bash script to automate src merge

--- swan_src/patch/create_patch_files.sh
+++ swan_src/patch/create_patch_files.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-stock_dir="/source/swan-src/swan_src/ftn_stock/"
-msl_dir="/source/swan-src/swan_src/ftn_msl/"
-diff_dir="/source/swan-src/swan_src/patch/"
+
+BASEDIR=/source/swan-src/swan_src
+
+stock_dir=$BASEDIR/ftn_stock
+msl_dir=$BASEDIR/ftn_msl
+diff_dir=$BASEDIR/patch
 
 ## -------- Generate patch files for all modified files:
-files=("abc" "def")
 files=("agioncmd.ftn90" "nctablemd.ftn90" "ocpids.ftn" "swanmain.ftn" "swanout1.ftn" \
        "SwanSpectPart.ftn" "swmod1.ftn" "swn_outnc.ftn90")
 for f in ${files[@]}; do
-  echo $f
-  diff -u -Z -B -p15 ${stock_dir}${f} ${msl_dir}${f} > ${diff_dir}${f}.patch
-  sed -i '1,2d' ${diff_dir}${f}.patch
+  echo "Creating patch for file $f"
+  diff -u -Z -B -p15 $stock_dir/$f $msl_dir/$f > ${diff_dir}/${f}.patch.2
 done
